4fe9fe78eb3873395f50912807bb7170114b8971
Swapnil Mali <maliswapnil982@gmail.com>
2019-05-06 19:22:40 +0530

    Added new feature 1. Now able to select only playable content

--- main_file.py
+++ main_file.py
@@ -1,7 +1,11 @@
+
+# @author swapmali
+
 import os
 import random
 import pandas as pd
 import re
+import sys
 
 
 def select_folder():
@@ -9,45 +13,48 @@ def select_folder():
     folder_names = list(folders_data['Folder_Name'])
     folder_paths = list(folders_data['Folder_path'])
 
-    # print(folder_names[2])
-    # print(folder_paths)
-
     ind = 0
     for index, row in folders_data.iterrows():
         ind = index + 1
         print(str(ind) + '. ' + row['Folder_Name'])
-    print(str(ind + 1) + '. ' + 'Select Randomly')
-    print('last value ind: ' + str(ind))
-    choice = int(input('Enter your choice no: '))
-    if 0 < choice <= ind:
-        # print('Apna Choice wala')   # debug
-        # print('\nPlaying from: {}'.format(folder_names[choice - 1]))
-        selected_folder_name = folder_names[choice - 1]
-        print('\nPlaying from Folder'
-              '\n{}'
-              '\n: {}'
-              '\n{}'.format('-' * (len(selected_folder_name) + 4), selected_folder_name,
-                            '-' * (len(selected_folder_name) + 4)))
+    ind += 1
+    print(str(ind) + '. ' + 'Select Randomly')
+    ind += 1
+    print(str(ind) + '. ' + 'Enter manually New Folder path')
 
-        return folder_paths[choice - 1]
-    else:
-        # print('Apna Random wala')   # debug
-        rand_choice = random.randint(1, ind - 1)
-        # print('\nPlaying from: {}'.format(folder_names[rand_choice - 1]))
-        selected_folder_name = folder_names[rand_choice - 1]
-        print('\nPlaying from Folder'
-              '\n{}'
-              '\n: {}'
-              '\n{}'.format('-' * (len(selected_folder_name) + 4), selected_folder_name,
-                            '-' * (len(selected_folder_name) + 4)))
+    try:
+        choice = int(input('Enter your choice no: '))
+        if 0 < choice <= (ind - 2):
+            selected_folder_name = folder_names[choice - 1]
+            print('\nPlaying from Folder '
+                  '\n{}'
+                  '\n** {} **'
+                  '\n{}'.format('*' * (len(selected_folder_name) + 6), selected_folder_name,
+                                '*' * (len(selected_folder_name) + 6)))
 
-        return folder_paths[rand_choice - 1]
+            return folder_paths[choice - 1]
+
+        elif choice == (ind - 1):
+            rand_choice = random.randint(1, ind - 1)
+            selected_folder_name = folder_names[rand_choice - 1]
+            print('\nPlaying from Folder '
+                  '\n{}'
+                  '\n** {} **'
+                  '\n{}'.format('*' * (len(selected_folder_name) + 6), selected_folder_name,
+                                '*' * (len(selected_folder_name) + 6)))
+
+            return folder_paths[rand_choice - 1]
+
+        else:
+            return input('\nEnter folder path:')
+
+    except ValueError:
+        print('\nPlease try again with valid choice..')
+        sys.exit()
 
 
-# play random songs from the directory
-def play_music(folder_path, song_list):
-    flg = 1
-    while flg == 1:
+def play_music(folder_path, song_list):  # play random songs from the directory
+    while True:
         try:
             now_playing_song = random.choice(song_list)
             song_name_length = len(now_playing_song)
@@ -67,13 +74,44 @@ def play_music(folder_path, song_list):
         os.system('"' + song_file_location + '"')
 
         song_list.remove(now_playing_song)
-        print(len(song_list))
+        print(str(len(song_list)) + ' songs remaining')
 
-        flg = int(input('\nPlay next or stop: (1/0)'))
+
+def make_song_list(folder_path):
+    pattern = '.{0,}\.(avi|mkv|mp4|wmv|mov|flv|ogm|vob|dat|ogg|asf|m1v|m2v|mp3|aac|flac|wma|rma)'
+    all_files_list = os.listdir(folder_path)
+    valid_music_files = []
+    invalid_music_files = []
+
+    for i in range(len(all_files_list)):
+        if re.match(pattern, all_files_list[i]):
+            valid_music_files.append(all_files_list[i])
+        else:
+            invalid_music_files.append(all_files_list[i])
+
+    no_all_files = len(all_files_list)
+    no_music_files = len(valid_music_files)
+
+    if no_all_files == no_music_files:
+        print('\n' + str(no_music_files) + ' Songs ')
+    else:
+        print('\n' + '=' * (13 + count_digit(no_all_files)) +
+              '\nTotal files: ' + str(no_all_files) +
+              '\nMusic files: ' + str(no_music_files) +
+              '\n' + '=' * (13 + count_digit(no_all_files)))
+
+    return valid_music_files
+
+
+def count_digit(n):
+    cnt = 0
+    while n > 0:
+        n = n // 10
+        cnt += 1
+    return cnt
 
 
 if __name__ == '__main__':
     folder_path = select_folder()  # selects a folder to play songs from
-    song_list = os.listdir(folder_path)  # get the list of all songs in a directory
-    print('\n' + str(len(song_list)) + ' Songs ')
+    song_list = make_song_list(folder_path)  # get the list of all songs in a directory
     play_music(folder_path, song_list)
